b0f171f395b14f61026b0ab97666ba3ee820564a
raheemamer <raheem.amer22@gmail.com>
2022-10-02 09:42:34 +0200

    All intro sections are done

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Dom manipulation
 
-// 2nd session - styling element
+// 2nd section - styling element
 
 const title = document.querySelector("#main-heading");
 
@@ -17,14 +17,66 @@ for (let i = 0; i < listItems.length; i++) {
 }
 console.log(listItems);
 
-// 3rd session - Creating Elements
+// 3rd section - Creating Elements
 const ul = document.querySelector("ul");
 
 const newElement = document.createElement("li")
 
-newElement.style.color = "black";
+newElement.style.fontSize = "1.5rem";
 
 ul.append(newElement);
 
-// 4 session - Modify text
+// 4th section - Modify text
 
+const firstListItem = document.querySelector(".list-items");
+
+// text only
+console.log(firstListItem.innerText);
+
+// display same as in html
+console.log(firstListItem.textContent);
+
+// display same as in html but with tags
+// there are security issues regarding using innerHTML as you don't want user input to be displayed in the inner html because it allows the user to control the javascript and html on the web page 
+// most cases you gonna user innerText
+console.log(firstListItem.innerHTML);
+
+newElement.innerText = "X-men";
+
+// 5th section - modify elements attributes and classes
+
+// how we deal with attributes [id]
+newElement.setAttribute("id", "main-heading");
+
+newElement.style.backgroundColor = "grey";
+
+newElement.removeAttribute("id");
+
+console.log(title.getAttribute("id"));
+
+// how we deal with attributes [classes]
+
+// newElement.setAttribute("class", "list-items");
+
+newElement.classList.add("list-items");
+
+newElement.classList.add("list-new");
+
+newElement.classList.remove("list-new");
+
+console.log(newElement.classList.contains("list-items"));
+
+// Remove element
+
+const newElement2 = document.createElement("h2");
+
+newElement2.innerText = "Hello";
+
+newElement2.style.color = "yellow";
+
+newElement2.style.fontSize = "2rem";
+
+
+ul.append(newElement2);
+
+newElement2.remove();
