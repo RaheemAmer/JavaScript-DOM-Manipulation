096bed708c2c352cd3347d55eebe67fae010100d
raheemamer <raheem.amer22@gmail.com>
2022-10-04 04:17:46 +0200

    Add Event listener training

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 // Event Listeners
+
+/*
+ Using onclick attribute in an inline way on the buttons
+
+ onclick = "alert('hello')"
+*/
+
+// element.addEventListener("click", function);
+
+const buttonTwo = document.querySelector("#btn-2");
+
+console.log(buttonTwo);
+
+function alertBox() {
+    alert("Hello from eventListener");
+}
+
+buttonTwo.addEventListener("click", alertBox);
+
+// MouserOver
+
+const newBgColor = document.querySelector("#btn-3");
+
+function changeBgcolor() {
+    newBgColor.style.backgroundColor = "red";
+}
+
+newBgColor.addEventListener("mouseover", changeBgcolor);
